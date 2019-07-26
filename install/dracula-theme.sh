33289d6d1e6d80f680ff780a1ffa3dd7ec89e0b4
Radovan Hajdukovic <hajdukovic.radovan@gmail.com>
2019-07-26 02:06:48 +0200

    add missing plug.vim

--- install/dracula-theme.sh
+++ install/dracula-theme.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# terminal
 git clone https://github.com/dracula/zsh.git ${ZSH_CUSTOM}/themes/dracula
 ln -s $ZSH_CUSTOM/themes/dracula/dracula.zsh-theme $ZSH/themes/dracula.zsh-theme
+
+# vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
