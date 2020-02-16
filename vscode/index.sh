e02fffee4e971708320fe633dab0af61e020e0fc
Radovan Hajdukovic <hajdukovic.radovan@gmail.com>
2020-02-16 01:33:39 +0100

    update vscode config

--- vscode/index.sh
+++ vscode/index.sh
@@ -5,22 +5,25 @@ source ../utils/utils.sh
 cp -r ./.vscode ~/.vscode
 cp ./settings.json  ~/Library/Application\ Support/Code/User/
 cp ./keybindings.json  ~/Library/Application\ Support/Code/User/
+cp -r ./snippets  ~/Library/Application\ Support/Code/User/
 e_success "VSCODE settings copied"
 
 e_header "Installing extensions please wait..."
 
+code --install-extension dracula-theme.theme-dracula
+code --install-extension editorconfig.editorconfig
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension ms-azuretools.vscode-docker
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension eamodio.gitlens
-code --install-extension EditorConfig.EditorConfig
-code --install-extension eg2.tslint
-code --install-extension eg2.vscode-npm-script
-code --install-extension Equinusocio.vsc-material-theme
-code --install-extension eriklynd.json-tools
+code --install-extension kumar-harsh.graphql-for-vscode
+code --install-extension techer.open-in-browser
 code --install-extension esbenp.prettier-vscode
-code --install-extension formulahendry.auto-rename-tag
-code --install-extension leizongmin.node-module-intellisense
-code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension robertohuertasm.vscode-icons
-code --install-extension steoates.autoimport
+code --install-extension humao.rest-client
+code --install-extension jasonnutter.search-node-modules
+code --install-extension cssho.vscode-svgviewer
+code --install-extension jpoissonnier.vscode-styled-components
+code --install-extension redhat.vscode-yaml
+code --install-extension eg2.vscode-npm-script
 
 e_success "Extensions for VSC have been installed. Please restart your VSC."
