2bcd2a9f169f615eb6cb601b10b559a131cad940
Radovan Hajdukovic <hajdukovic.radovan@gmail.com>
2020-02-16 02:01:20 +0100

    add autosuggestions plugin and set zsh as default

--- oh-my-zsh/.zshrc
+++ oh-my-zsh/.zshrc
@@ -6,6 +6,9 @@ git clone https://github.com/dracula/zsh.git ${ZSH_CUSTOM}/themes/dracula
 ln -s $ZSH_CUSTOM/themes/dracula/dracula.zsh-theme $ZSH/themes/dracula.zsh-theme
 ZSH_THEME="dracula"
 
+# install autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 plugins=(
   git
   zsh-autosuggestions
--- oh-my-zsh/index.sh
+++ oh-my-zsh/index.sh
@@ -4,10 +4,18 @@ source ../utils/utils.sh
 
 ZSH=~/.oh-my-zsh
 
+# install zsh, registers zsh as a default shell
+brew install zsh
+ZSH_PATH=$(which zsh)
+grep -Fxq "$ZSH_PATH" /etc/shells || sudo bash -c "echo $ZSH_PATH >> /etc/shells"
+
+# make zsh default shell
+chsh -s "$ZSH_PATH" $USER
+
 if [ -d "$ZSH" ]; then
   e_warning "Oh My Zsh is already installed. skipping.."
 else
-  curl -L http://install.ohmyz.sh | sh
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
 # To install ZSH themes & aliases
